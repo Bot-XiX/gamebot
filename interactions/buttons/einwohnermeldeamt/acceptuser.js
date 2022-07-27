88b99c7647e578e71581338b00695f3845810bdc
lucifer <l@thiemeonline.de>
2022-07-27 17:38:59 +0200

    Color change after accepting / declining users

--- interactions/buttons/einwohnermeldeamt/acceptuser.js
+++ interactions/buttons/einwohnermeldeamt/acceptuser.js
@@ -11,7 +11,7 @@ module.exports = {
 
 * @param {Object} interaction The Interaction Object of the command.
 */
-  async execute (interaction) {
+  async execute(interaction) {
     const target = interaction.guild.members.cache.get(interaction.message.content.split('\n')[0].slice(2).slice(0, -1))
     target.roles.add(interaction.guild.roles.cache.find(r => r.name === 'Tourist'))
     interaction.reply({ content: 'Tourist role added.', ephemeral: true })
@@ -26,6 +26,12 @@ module.exports = {
         content: `${interaction.user.tag} hat ${target.user} die Rolle \`Tourist\` hinzugefügt`
       })
     }
-    interaction.message.edit({ components: [] })
+    const embed = (interaction.message.embeds[0])
+    embed.data.color = 3066993
+    try {
+      await interaction.message.edit({ components: [], embed: [embed] })
+    } catch {
+      return null
+    }
   }
 }
