897a0fbfdebd414d403faf8bdf649c29fc0e1ec6
Lucifer <l@thiemeonline.de>
2022-08-08 14:43:58 +0200

    Update TicketTool
    Co-authored-by: Felix Limbach <schblondie@gmail.com>

--- events/onRateLimit.js
+++ events/onRateLimit.js
@@ -1,20 +1,18 @@
+const { RateLimitError } = require('discord.js');
 /**
  * @file Ready Event File.
  * @since 1.0.0
 */
 
 module.exports = {
-  name: 'ready',
-  once: true,
+  name: 'rateLimit',
 
   /**
    * @description Executes the block of code when client is ready (bot initialization)
    * @param {Object} client Main Application Client
    */
 
-  execute (client) {
-    // eslint-disable-next-line no-console
-    console.log(`Ready! Logged in as ${client.user.tag}`)
-    // dashboard.run()
+  execute (info) {
+    console.log(1);
   }
 }
--- interactions/buttons/tickets/closeticket.js
+++ interactions/buttons/tickets/closeticket.js
@@ -1,41 +1,45 @@
-const { EmbedBuilder, ActionRowBuilder, ButtonStyle, ButtonBuilder } = require('discord.js')
-const prev = require('./createadminticket')
+const { EmbedBuilder, ActionRowBuilder, ButtonStyle, ButtonBuilder, time } = require('discord.js')
+// const prev1 = require('./createadminticket')
+// const prev2 = require('./createcomplaint')
 
 /**
- * @file Button interaction: closeticket
+ * @file Button interaction: closeTicket
 
  * @since 1.0.0
 */
 module.exports = {
-  id: 'closeticket',
+  id: 'closeTicket',
   /**
-  * @description Executes when the button with ID closeticket is called.
+  * @description Executes when the button with ID closeTicket is called.
   * @param {Object} interaction The Interaction Object of the command.
   */
   async execute (interaction) {
-    const channel = interaction.guild.channels.cache.get(interaction.channel.id)
+    const channel = interaction.channel
     const channelName = channel.name
     const target = interaction.member
     const targetName = target.user.toString()
     const targetAvatar = target.user.avatarURL()
-    channel.setParent(interaction.guild.channels.cache.get('1005821144575774750'))
-    channel.setName(`🔒-${channelName}`)
+    channel.setParent(await interaction.guild.channels.cache.get('1005821144575774750'))
+    function wait (ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    var startTime = performance.now()
+    let endTime
+    channel.setName(`🔒-${channelName}`).then(async () => {
+      endTime = performance.now()
+    })
+    let timeTaken
+    setTimeout(async () => {
+      timeTaken = endTime - startTime
+    }, 1000)
+    await wait(2000)
     const closedEmbed = new EmbedBuilder()
       .setTitle('Ticket geschlossen')
-      // .setURL('')
       .setAuthor({
         name: target.user.username,
         iconURL: targetAvatar
       })
       .setDescription(`Das Ticket wurde durch ${targetName} geschlossen.`)
-      // .setThumbnail('')
-      // .setColor('')
-      // .addFields(
-      //    { name: '', value: '' },
-      //    { name: '', value: '' },
-      // )
-      // .addField('Inline field title', 'Some value here', true)
-      // .setImage('')
       .setTimestamp()
       .setFooter({
         text: `Ticket | ${target.user.id}`
@@ -53,9 +57,13 @@ module.exports = {
           .setStyle(ButtonStyle.Danger) // Primary, Secondary, Success, Danger, Link
           .setEmoji('🗑️') // If you want to use an emoji
       )
-    interaction.reply({ embeds: [closedEmbed], components: [ticketControls] })
+    await interaction.reply({ embeds: [closedEmbed], components: [ticketControls] })
     interaction.message.edit({
       components: []
     })
+    if (isNaN(timeTaken)) {
+      closedEmbed.setDescription(`Das Ticket wurde durch ${targetName} geschlossen.\n⚠️ Achtung, der Channel-Name konnte aufgrund der Discord-Zeitbegrenzung nicht umbenannt werden. ⚠️`)
+      interaction.editReply({ embeds: [closedEmbed] })
+    }
   }
 }
