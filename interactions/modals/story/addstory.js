ff58b863aee488e2131f3d2bccb37f357baacd72
Felix Limbach <schblondie@gmail.com>
2022-09-18 21:55:22 +0200

    Fixed modal

--- interactions/modals/story/addstory.js
+++ interactions/modals/story/addstory.js
@@ -11,7 +11,7 @@ module.exports = {
 * @param {Object} interaction The Interaction Object of the command.
 */
   async execute (interaction) {
-    const satz = interaction.data.values.getTextInputValue('satz')
+    const satz = interaction.fields.getTextInputValue('satz')
     const channel = interaction.guild.channels.cache.get('1018644639504412672')
     const channel2 = interaction.guild.channels.cache.get('1020035934785380453')
     const row = new ActionRowBuilder().addComponents(
