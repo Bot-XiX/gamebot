eb4f2d14dc3445e1166b92a57d73287dbb67df87
Felix Limbach <schblondie@gmail.com>
2022-09-22 00:25:11 +0200

    Added logs for testing

--- interactions/select-menus/empfangtools/incomplete.js
+++ interactions/select-menus/empfangtools/incomplete.js
@@ -15,86 +15,89 @@ module.exports = {
 * @param {Object} interaction The Interaction Object of the command.
 */
   async execute (interaction) {
-    try {
-      interaction.deferReply({ ephemeral: true })
-      const newRow = await new ActionRowBuilder().addComponents(prev.prev.interaction.message.components[0].components[3])
-      await prev.prev.interaction.message.edit({ components: [] })
-      const db = getDatabase()
-      const id = interaction.guild.id
-      const empfangslog = interaction.member.guild.channels.cache.get(JSON.stringify(await get(ref(db, id + '/einwohnermeldeamt/config/eLog'))).slice(1).slice(0, -1))
-      const target = prev.prev.target
-      // Sends a message about the action to the log
-      empfangslog.send({
-        content: `${interaction.user.tag} hat die Vorstellung von ${target.user} als unvollständig markiert`
-      })
-      module.exports.prev = target
-      const serverIcon = interaction.guild.iconURL()
-      const valuelist = []
-      for (let i = 0; i < interaction.values.length; i++) {
-        switch (interaction.values[i]) {
-          case 'name':
-            valuelist.push('Name')
-            break
-          case 'age':
-            valuelist.push('Alter')
-            break
-          case 'place':
-            valuelist.push('Wohnort')
-            break
-          case 'sexuality':
-            valuelist.push('Sexualität')
-            break
-          case 'gender':
-            valuelist.push('Geschlecht/Pronomen')
-            break
-          case 'hobbies':
-            valuelist.push('Hobbies')
-            break
-        }
+    interaction.deferReply({ ephemeral: true })
+    const newRow = await new ActionRowBuilder().addComponents(prev.prev.interaction.message.components[0].components[3])
+    await prev.prev.interaction.message.edit({ components: [] })
+    const db = getDatabase()
+    const id = interaction.guild.id
+    const empfangslog = interaction.member.guild.channels.cache.get(JSON.stringify(await get(ref(db, id + '/einwohnermeldeamt/config/eLog'))).slice(1).slice(0, -1))
+    const target = prev.prev.target
+    // Sends a message about the action to the log
+    empfangslog.send({
+      content: `${interaction.user.tag} hat die Vorstellung von ${target.user} als unvollständig markiert`
+    })
+    module.exports.prev = target
+    const serverIcon = interaction.guild.iconURL()
+    const valuelist = []
+    for (let i = 0; i < interaction.values.length; i++) {
+      switch (interaction.values[i]) {
+        case 'name':
+          valuelist.push('Name')
+          break
+        case 'age':
+          valuelist.push('Alter')
+          break
+        case 'place':
+          valuelist.push('Wohnort')
+          break
+        case 'sexuality':
+          valuelist.push('Sexualität')
+          break
+        case 'gender':
+          valuelist.push('Geschlecht/Pronomen')
+          break
+        case 'hobbies':
+          valuelist.push('Hobbies')
+          break
       }
-      prev.prev.interaction.editReply({
-        content: `Deine Auswahl (${valuelist}) wurde gespeichert.`,
-        components: []
+    }
+    console.log('1')
+    prev.prev.interaction.editReply({
+      content: `Deine Auswahl (${valuelist}) wurde gespeichert.`,
+      components: []
+    })
+    console.log('2')
+    const incompleteMsgEmbed = new EmbedBuilder()
+      .setTitle('QueerCity Verifizierung')
+      .setThumbnail(serverIcon)
+      .setDescription(JSON.stringify(await get(ref(db, interaction.member.guild.id + '/einwohnermeldeamt/config/incompleteMsg'))).slice(1).slice(0, -1).replaceAll('\\n', '\n') + '\n**Folgende Angaben fehlten oder waren falsch:**\n' + valuelist.toString().replaceAll(',', '\n'))
+      .setFooter({ text: 'QueerCity Verifizierungssystem', iconURL: serverIcon })
+    const newmsg = (prev.prev.interaction.message.content) + '\nUnvollständig markiert durch: ' + String(interaction.user)
+    console.log('3')
+    const embed = prev.prev.embed
+    console.log('4')
+    embed.data.color = 15158332
+    console.log('5')
+    try {
+      await target.user.send({
+        embeds: [incompleteMsgEmbed]
       })
-      const incompleteMsgEmbed = new EmbedBuilder()
-        .setTitle('QueerCity Verifizierung')
-        .setThumbnail(serverIcon)
-        .setDescription(JSON.stringify(await get(ref(db, interaction.member.guild.id + '/einwohnermeldeamt/config/incompleteMsg'))).slice(1).slice(0, -1).replaceAll('\\n', '\n') + '\n**Folgende Angaben fehlten oder waren falsch:**\n' + valuelist.toString().replaceAll(',', '\n'))
-        .setFooter({ text: 'QueerCity Verifizierungssystem', iconURL: serverIcon })
-      const newmsg = (prev.prev.interaction.message.content) + '\nUnvollständig markiert durch: ' + String(interaction.user)
-      const embed = prev.prev.embed
-      embed.data.color = 15158332
+      console.log('6')
+      interaction.editReply({
+        content: 'Vorstellung als unvollständig markiert.',
+        ephemeral: true
+      })
+      console.log('7')
       try {
-        await target.user.send({
-          embeds: [incompleteMsgEmbed]
-        })
-        interaction.editReply({
-          content: 'Vorstellung als unvollständig markiert.',
-          ephemeral: true
-        })
-
-        try {
-          await prev.prev.interaction.message.edit({
-            content: newmsg,
-            components: [],
-            embeds: [embed]
-          })
-        } catch {
-          return null
-        }
-      } catch (e) {
-        interaction.editReply({
-          content: 'User akzeptiert keine Nachrichten.\nSetze den User stattdessen in VE2',
-          ephemeral: true
-        })
-        prev.prev.interaction.message.edit({
-          content: newmsg + '\n*User akzeptiert keine Nachrichten.*',
-          components: [newRow],
+        await prev.prev.interaction.message.edit({
+          content: newmsg,
+          components: [],
           embeds: [embed]
         })
+        console.log('8')
+      } catch {
+        return null
       }
     } catch (e) {
-      console.log(e)
+      interaction.editReply({
+        content: 'User akzeptiert keine Nachrichten.\nSetze den User stattdessen in VE2',
+        ephemeral: true
+      })
+      prev.prev.interaction.message.edit({
+        content: newmsg + '\n*User akzeptiert keine Nachrichten.*',
+        components: [newRow],
+        embeds: [embed]
+      })
     }
   }
 }
